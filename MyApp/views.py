348d568a50e6c589f6ada6681959cdb827ac0dbf
Dhruvil1304 <modidhruvil42@gmail.com>
2023-10-22 16:35:52 -0400

    Added a Bootstrap template and started with models and logics

--- MyApp/views.py
+++ MyApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Feature
 
 
 def index(request):
@@ -8,7 +9,23 @@ def index(request):
     #     "age": 22,
     #     "nationality": "Indian"
     # }
-    return render(request, 'index.html')
+    feature1 = Feature()
+    feature1.id = 1
+    feature1.name = "Fast"
+    feature1.details = "Our service is very fast"
+
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = "Reliable"
+    feature2.details = "Our service is very reliable"
+
+    feature3 = Feature()
+    feature3.id = 1
+    feature3.name = "Easy access"
+    feature3.details = "Our service is very easy to access"
+
+    features = [feature1, feature2, feature3]
+    return render(request, 'index.html', {'features': features})
 
 
 def counter(request):
